ef193f6651b254613de16304a8d35c1b7c97340c
merlinymy <lordhorcrux@gmail.com>
2024-09-03 21:22:53 -0500

    finish right side bar add git like tracking system

--- javascript.js
+++ javascript.js
@@ -2,6 +2,62 @@
 let mainDivs,
 mainDivH4s;
 
+let checkInSqaures, checkInCol;
+
+checkInSqaures = document.querySelector(".check-in-col1").children;
+// console.log(checkInSqaures);
+checkInCol = document.querySelectorAll(".check-in-col");
+console.log(checkInCol);
+let monthArr = ['Dec', 'Nov', 'Oct', 'Sep', 'Aug', 'Jul', 'Jun', 'May',
+    'Apr', 'Mar', 'Feb', 'Jan'];
+
+for(let i = 4; i <= checkInSqaures.length; i++) {
+    if (i % 4 === 0) {
+        checkInSqaures[i].textContent = monthArr.shift();
+        checkInSqaures[i].classList.add('month-cell');
+    }
+}
+
+Array.from(checkInCol).slice(1).forEach(element => {
+    console.log('in checkIncol');
+    console.log(element);
+    Array.from(element.children).slice(1).map(ele => {
+        let randomInt = getRandomInt(4);
+        console.log(randomInt)
+        applyRandomClr(ele, randomInt);
+    })
+});
+
+function applyRandomClr(ele, randomInt) {
+    // console.log(randomInt);
+    switch (randomInt) {
+        case 0:
+            ele.classList.add("green-1");
+            break;
+        case 1:
+            ele.classList.add("green-2");
+            break;
+        case 2:
+            ele.classList.add("green-3");
+            break;
+        case 3:
+            ele.classList.add("green-4");
+            break;
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+
+
+
+
+
+
+
 mainDivs = document.querySelector(".main").children;
 // console.log(mainDivs);
 
